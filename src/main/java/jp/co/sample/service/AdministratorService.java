3f6e407edf209862b1ea324702f3835c76f4a019
shiokawaryoko <ryoko.shiokawa@rakus-partners.co.jp>
2021-05-10 15:33:46 +0900

    forth commit

---
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,21 @@
+package jp.co.sample.service;
+
+/**
+ * 管理者情報を操作するサービス
+ * 
+ *  @author shiokawaryoko
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.repository.AdministratorRepository;
+
+@Service
+@Transactional
+public class AdministratorService {
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	
+}
